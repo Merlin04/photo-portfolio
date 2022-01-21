6d3f19608e8482ba9b495edd2315ab6205624add
Benjamin Smith <Merlin04@users.noreply.github.com>
2022-01-21 17:20:00 +0000

    Fix bugs routing

--- pages/[[...item]].tsx
+++ pages/[[...item]].tsx
@@ -8,7 +8,6 @@ import { Button, Card, CardHeader, Chip, DialogContent, DialogTitle, Link, Modal
 import Image from 'next/image';
 import theme from '../src/theme';
 import Marquee from "../src/MarqueePolyfill";
-import NextLink from "../src/Link";
 import Masonry from "@mui/lab/Masonry";
 import Head from 'next/head';
 import { useRouter } from 'next/router';
@@ -24,6 +23,8 @@ type IndexProps = {
 let hasSetMutationObserver = false;
 
 const COOL_TIMING_FUNCTION = "cubic-bezier(.17,.84,.44,1)";
+
+const setPoofCalculatedRadius = () => document.documentElement.style.setProperty("--poof-calculated-radius", Math.sqrt(document.body.clientWidth ** 2 + document.body.clientHeight ** 2).toString() + "px");
 
 export default function Index({ data, randomPos, randomShadow }: IndexProps) {
   const router = useRouter();
@@ -53,6 +54,7 @@ export default function Index({ data, randomPos, randomShadow }: IndexProps) {
 
   const [exiting, setExiting] = React.useState<boolean>(false);
   const modalRef = React.useRef<HTMLDivElement>(null);
+  // const modalRootRef = React.useRef<HTMLDivElement>(null);
 
   // React.useEffect(() => {
   //   history.pushState({}, "", openItem ? "/" + encodeURIComponent(openItem.title) : "/");
@@ -64,6 +66,8 @@ export default function Index({ data, randomPos, randomShadow }: IndexProps) {
         // setOpenItem(undefined);
         router.push("/", undefined, { shallow: true });
         setExiting(false);
+        // The effect handler would do this but we need to do it in the same render to avoid flashes of the modal
+        setOpenItem(undefined);
         modalRef.current?.removeEventListener("animationend", handler);
       }
 
@@ -108,59 +112,60 @@ export default function Index({ data, randomPos, randomShadow }: IndexProps) {
       <Container maxWidth="lg" sx={{
         marginY: theme.spacing(6)
       }}>
-      <Masonry columns={3} spacing={4} ref={masonryRef}>
-        {data.items.map((item, index) => (
-          <Link key={index} href={`/${encodeURIComponent(item.title)}`} sx={{
-            visibility: masonryDone ? "inherit" : "hidden"
-          }} onClick={e => {
-            e.preventDefault();
-            document.documentElement.style.setProperty("--poof-x", e.clientX.toString() + "px");
-            document.documentElement.style.setProperty("--poof-y", e.clientY.toString() + "px");
-            // Sadly CSS doesn't support square root
-            document.documentElement.style.setProperty("--poof-calculated-radius", Math.sqrt(document.body.clientWidth ** 2 + document.body.clientHeight ** 2).toString() + "px");
-            router.push(`/${encodeURIComponent(item.title)}`, undefined, { shallow: true });
-            //setOpenItem(item);
-          }}>
-            <Card variant="outlined" sx={{
-              // TODO: maybe keep this? bgcolor: item.content.Background.value ?? undefined,
-              display: "inline-block",
-              width: "350px",
-              maxWidth: "100%",
-              position: "relative",
-              top: randomPos[index][0],
-              left: randomPos[index][1],
-              boxShadow: `${randomShadow[index][0]}px ${randomShadow[index][1]}px ${item.content.Background.value ?? "black"}`,
-              // borderWidth: "4px"
-              borderStyle: "ridge",
-              borderWidth: "4mm",
-              // a6a6a691
-              borderColor: "#97979791",
-              ":hover": {
-                top: randomPos[index][0] - 10,
-                left: randomPos[index][1] - 10,
-                boxShadow: `${randomShadow[index][0] + 20}px ${randomShadow[index][1] + 20}px ${item.content.Background.value ?? "black"}`
-              },
-              transform: masonryDone ? "inherit" : "scale(0)",
-              transition: `all 1s ${COOL_TIMING_FUNCTION}`
+        <Masonry columns={3} spacing={4} ref={masonryRef}>
+          {data.items.map((item, index) => (
+            <Link key={index} href={`/${encodeURIComponent(item.title)}`} sx={{
+              visibility: masonryDone ? "inherit" : "hidden"
+            }} onClick={e => {
+              e.preventDefault();
+              document.documentElement.style.setProperty("--poof-x", e.clientX.toString() + "px");
+              document.documentElement.style.setProperty("--poof-y", e.clientY.toString() + "px");
+              // Sadly CSS doesn't support square root
+              setPoofCalculatedRadius();
+              router.push(`/${encodeURIComponent(item.title)}`, undefined, { shallow: true });
+              //setOpenItem(item);
             }}>
-              <CardHeader title={item.title} subheader={item.content.Date.value} titleTypographyProps={{
-                variant: "h4"
-              }} />
-              <Box sx={{
+              <Card variant="outlined" sx={{
+                // TODO: maybe keep this? bgcolor: item.content.Background.value ?? undefined,
+                display: "inline-block",
+                width: "350px",
+                maxWidth: "100%",
                 position: "relative",
-                height: "300px"
+                top: randomPos[index][0],
+                left: randomPos[index][1],
+                boxShadow: `${randomShadow[index][0]}px ${randomShadow[index][1]}px ${item.content.Background.value ?? "black"}`,
+                // borderWidth: "4px"
+                borderStyle: "ridge",
+                borderWidth: "4mm",
+                // a6a6a691
+                borderColor: "#97979791",
+                ":hover": {
+                  top: randomPos[index][0] - 10,
+                  left: randomPos[index][1] - 10,
+                  boxShadow: `${randomShadow[index][0] + 20}px ${randomShadow[index][1] + 20}px ${item.content.Background.value ?? "black"}`
+                },
+                transform: masonryDone ? "inherit" : "scale(0)",
+                transition: `all 1s ${COOL_TIMING_FUNCTION}`
               }}>
-                <Image
-                  src={process.env.NEXT_PUBLIC_BEROWRA_INST + "/file/" + item.content.Images.value[0]}
-                  alt="Assignment picture"
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </Box>
-            </Card>
-          </Link>
-        ))}
-      </Masonry></Container>
+                <CardHeader title={item.title} subheader={item.content.Date.value} titleTypographyProps={{
+                  variant: "h4"
+                }} />
+                <Box sx={{
+                  position: "relative",
+                  height: "300px"
+                }}>
+                  <Image
+                    src={process.env.NEXT_PUBLIC_BEROWRA_INST + "/file/" + item.content.Images.value[0]}
+                    alt="Assignment picture"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </Box>
+              </Card>
+            </Link>
+          ))}
+        </Masonry>
+      </Container>
 
       {openItem && (
         <Head>
@@ -170,42 +175,53 @@ export default function Index({ data, randomPos, randomShadow }: IndexProps) {
         </Head>
       )}
 
-      <Modal ref={modalRef} open={Boolean(openItem)} sx={{
-        animationName: exiting ? "unpoof" : "poof",
-        animationDuration: "0.5s",
-        animationTimingFunction: COOL_TIMING_FUNCTION
-      }} BackdropProps={{
-        sx: {
-          bgcolor: "white"
-        }
-      }}>
-        <Paper variant="outlined" sx={{
-          //padding: theme.spacing(4),
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          borderWidth: "0.5rem"
-        }}>
-          <Typography variant="h3" sx={{
-            padding: "1rem",
-            bgcolor: openItem?.content.Background.value ?? "inherit"
-          }}>{openItem?.title}</Typography>
-          <Box sx={{
-            padding: "1rem"
+      {/* <Box ref={modalRootRef}> */}
+        <Modal
+          ref={modalRef}
+          // disablePortal={typeof window === "undefined"}
+          // container={typeof window === "undefined" ? () => modalRootRef.current : undefined}
+          open={Boolean(openItem)}
+          sx={{
+            animationName: exiting ? "unpoof" : "poof",
+            animationDuration: "0.5s",
+            animationTimingFunction: COOL_TIMING_FUNCTION,
+            // clipPath: (exiting || !openItem) ? "circle(0 at 0 0)" : "inherit"
+          }}
+          BackdropProps={{
+            sx: {
+              bgcolor: "white",
+              transition: "inherit !important"
+            }
+          }}
+        >
+          <Paper variant="outlined" sx={{
+            //padding: theme.spacing(4),
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            borderWidth: "0.5rem"
           }}>
-            <Chip label={openItem?.content.Date.value} variant="outlined" sx={{
-              fontSize: 20,
-              borderRadius: 0
-            }} />
-          </Box>
-          <Button onClick={() => {
-            setExiting(true);
-            // TODO: move route change back here?
-          }}>Close</Button>
-        </Paper>
-      </Modal>
-      
+            <Typography variant="h3" sx={{
+              padding: "1rem",
+              bgcolor: openItem?.content.Background.value ?? "inherit"
+            }}>{openItem?.title}</Typography>
+            <Box sx={{
+              padding: "1rem"
+            }}>
+              <Chip label={openItem?.content.Date.value} variant="outlined" sx={{
+                fontSize: 20,
+                borderRadius: 0
+              }} />
+            </Box>
+            <Button onClick={() => {
+              setPoofCalculatedRadius();
+              setExiting(true);
+              // TODO: move route change back here?
+            }}>Close</Button>
+          </Paper>
+        </Modal>
+      {/* </Box> */}
     </>
   );
 };
